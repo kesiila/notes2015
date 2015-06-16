0c3550e8be5bc842df9857d4d057a10c980b5fb0
kesiila <76313740@qq.com>
2015-06-16 19:09:32 +0800

    t

---
+++ multiThread/multiThread.md
@@ -0,0 +1,3 @@
+###### multiply thread
+1. 多线程没有父子关系.
+2. Thread 对应 OSahing的为线程
---
+++ sort/ReadTestDataFromFile.java
@@ -0,0 +1,11 @@
+package  sort;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public interface ReadTestDataFromFile {
+    public default List<Object> readData() {
+        // need to add logic
+        return new ArrayList<Object>();
+    }
+}
