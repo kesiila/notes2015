279903770df6afc52d6608bd7e719d183ac3fb2d
kesiila <76313740@qq.com>
2016-06-21 16:35:13 +0800

    remove dada-edu-dev

--- dada-edu-dev/bootstrap/webapp/build/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wuyunge on 15/11/30.
- */
-"use strict";
-
-module.exports = {
-        root: __dirname + '/webapp'
-    };
--- dada-edu-dev/bootstrap/webapp/build/index/bootstrap.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var config = require('../config');
-var root = config['root'];
-
-global.$ = global.jQuery = require('jquery');
-require('bootstrap-sass');
-require('bootstrap-sass/assets/stylesheets/_bootstrap.scss');
